30431c286852aaa1cdcd2e3b79fe577994effa0e
dachi67 <dachi67@users.noreply.github.com>
2024-11-10 16:45:25 +0400

    commits

---
+++ day 04/classwork/classwork.py
@@ -0,0 +1,29 @@
+#no classwork
+value = 5.70
+quantity = 3
+print(value * quantity)
+
+my_age = 13
+sis_age = 14
+print("this is my age after 10 years:", my_age)
+print("this is my sister's age after 10 years:", sis_age)
+
+num1 = int(input("enter number: "))
+num2 = int(input("enter number: "))
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+print(num1 / num2)
+
+
+name = "Andria"
+age = 13
+print("my name is " + name + ",", "my age is " + str(age) + ".")
+
+
+parent_age = 39
+my_age = 13
+print((parent_age + 10) - (my_age + 10))
+
+
+#no classwork 
